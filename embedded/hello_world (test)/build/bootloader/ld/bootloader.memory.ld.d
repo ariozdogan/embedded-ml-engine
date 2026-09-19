ee9fe92698a8b618c1951988d23b7b9a19e437db
Ari Ozdogan <ari.ozdogan@gmail.com>
2026-09-19 16:09:27 -0400

    added esp framework

---
+++ embedded/hello_world (test)/build/bootloader/ld/bootloader.memory.ld.d
@@ -0,0 +1,3 @@
+/Users/ariozdogan/tinyml-inference-engine/embedded/hello_world/build/bootloader/ld/bootloader.memory.ld: \
+ /Users/ariozdogan/esp/esp-idf/components/bootloader/subproject/main/ld/esp32s3/bootloader.memory.ld.in \
+ /Users/ariozdogan/tinyml-inference-engine/embedded/hello_world/build/bootloader/config/sdkconfig.h
